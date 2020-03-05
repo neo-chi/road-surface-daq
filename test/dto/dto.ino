389e790e2eec90420a529fcb0c622786f9922947
reece chimento <reecechimento@gmail.com>
2020-03-05 13:42:59 -0600

    test(dto): verify dto.h base structure toString()
    All base structure toString methods have been tested and verified
    correct.
    
    Date: YYYYMMDD
    Time: HHMMSS
    Datetime: YYMMDD,HHMMSS

--- test/dto/dto.ino
+++ test/dto/dto.ino
@@ -7,8 +7,13 @@ const uint8_t day = 3;
 Date date = {year, month, day};
 
 // Test Time Construction
+const uint8_t hour = 12;
+const uint8_t minute = 8;
+const uint8_t second = 19;
+Time _time = {hour, minute, second};
 
 // Test Datetime
+Datetime datetime = {date, _time};
 
 // Test Impact
 
@@ -22,7 +27,26 @@ void setup() {
   delay(1000);  // UNIX
   Serial.begin(115200);  // Debugging
 
+  // Display date
+  Serial.println("TEST: \t\t\t date.toString()");
+  Serial.println("EXPECTED: \t 20200303");
+  Serial.print("ACTUAL: \t\t ");
   Serial.println(date.toString());
+  Serial.println();
+
+  // Display time
+  Serial.println("TEST: \t\t\t time.toString()");
+  Serial.println("EXPECTED: \t 120819");
+  Serial.print("ACTUAL: \t\t ");
+  Serial.println(_time.toString());
+  Serial.println();
+
+  // Display datetime
+  Serial.println("TEST: \t\t\t datetime.toString()");
+  Serial.println("EXPECTED: \t 20200303,120819");
+  Serial.print("ACTUAL: \t\t ");
+  Serial.println(datetime.toString());
+  Serial.println();
 }
 
 void loop() {
