4bda30715c0efa209c38dfb0030103537d64db77
reece chimento <reecechimento@gmail.com>
2020-03-08 17:24:52 -0500

    test(travel): verified log and upload methods
    The Travel class has passed the following tests:
    
    * toLogString()
    * toUploadString()

---
+++ test/travel/travel.ino
@@ -0,0 +1,43 @@
+#include "travel.h"
+#include "rtest.h"
+
+void setup() {
+  delay(1000);  // UNIX
+  Serial.begin(115200);  // Debugging
+
+  // Setup datetime
+  const uint16_t year = 2020;
+  const uint8_t month = 1;
+  const uint8_t day = 6;
+  Date date{year, month, day};
+  const uint8_t hour = 3;
+  const uint8_t minute = 29;
+  const uint8_t second = 4;
+  Time time{hour, minute, second};
+  Datetime datetime{date, time};
+
+  // Setup location
+  const float latitude = 35.0687;
+  const float longitude = -83.229100;
+  Location location{latitude, longitude};
+
+  // Setup Travel
+  Travel travel{datetime, location};
+  StringTest toLogString{
+    "Travel.toLogString()",
+    "2020-01-06,03:29:04,35.068699,-83.229103",
+    travel.toLogString()
+  };
+  toLogString.Execute();
+
+  StringTest toUploadString{
+    "Travel.toUploadString()",
+    "datetime=2020-01-06+03\\%3A29\\%3A04&latitude=35.068699&longitude=-83.229103",
+    travel.toUploadString()
+  };
+  toUploadString.Execute();
+}
+
+void loop() {
+  // Nothing to do here.
+}
