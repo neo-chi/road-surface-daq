71ea7b82860aa0397d48bc2086194c6d31f4ada8
reece chimento <reecechimento@gmail.com>
2020-03-10 02:59:53 -0500

    src(uploader): initialize class
    The Uploader class is reponsible for uploading data to a PHP POST
    script. This will be achieved using an HTTP request via a
    url-encoded-form.
    The Uploader provides a single interface, Upload() that accepts a
    location header to the php script and a String of data to be
    uploaded.

---
+++ src/uploader/uploader.h
@@ -0,0 +1,19 @@
+#ifndef _ROAD_SURFACE_DAQ_UPLOADER_H
+#define _ROAD_SURFACE_DAQ_UPLOADER_H
+
+#include "Arduino.h"
+// @reqiure WiFiClient.h
+// @require HttpClient.h
+
+class Uploader {  // @note Implements Observable
+  public:
+    void Upload(const String& location_header, const String& post_data);
+  private:
+    // WiFiClient wifi;
+    // HttpClient http;
+    // bool EnableWiFi();
+    // bool DisableWiFi();
+    // bool WaitForWiFiConnection(long timeout);
+};
+
+#endif
