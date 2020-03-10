37a9383113ac6e8ea4c82eb6dc8d4da89cff3a5f
reece chimento <reecechimento@gmail.com>
2020-03-10 02:57:29 -0500

    src(storage): initialize class
    The Storage class is responsible for managing local storage, and
    reading and writing files. Data must be written or read to a buffer of
    a specifed length. Upon constructing the Storage class, the SD card will
    be initialized and will be referenced by the file-system in a private
    context, reducing the interface function's complexity.

---
+++ src/storage/storage.h
@@ -0,0 +1,29 @@
+#ifndef _ROAD_SURFACE_DAQ_STORAGE_H
+#define _ROAD_SURFACE_DAQ_STORAGE_H
+
+#include "Arduino.h"
+// @require FS.h
+// @require SD.h
+// @require SPI.h
+
+class Storage {  // @interface Implements Observable
+  public:
+    Storage();  // initialize SD, fs = SD
+    void read(const String& path, String *buffer, uint8_t buffer_len);  // buffer is output
+    void write(const String& path, String *buffer, uint8_t buffer_len);  // buffer is input
+    bool mkdir(const String& path);
+    bool rename(const String& path_from, const String& path_to);
+    bool remove(const String& path);
+  private:
+    // FS::fs &fs
+      // fs.mkdir
+      // fs.rename --> rename a file or move a file
+      // fs.remove
+      // fs.open
+      // fs.read
+      // fs.write
+      // fs.close
+    // SD.begin(ROOT_DIR = "/")
+};
+
+#endif
