23c77ad39a4c68c97b78ae544c041ca3a1aefe71
reece chimento <reecechimento@gmail.com>
2020-03-12 00:47:32 -0500

    src(data_collector): add data_collector event
    The data collector now supports updating it's mediator with a
    data_collector_event.
    
    The data_collector_event is an enum defining the various events that may
    thrown by the data_collector, which are caught and handled by the
    Mediator via its Upload method.

--- src/data_collector/data_collector.cpp
+++ src/data_collector/data_collector.cpp
@@ -1,9 +1,18 @@
 #include "data_collector.h"
+#include "mediator.h"
 
-// The Data collector class is responsible for getting data from external
-// sensors in the form of a String, or a String buffer.
 DataCollector::DataCollector() {
   Serial.println("Constructed a DataCollector");
+}
+
+void DataCollector::Attach(Mediator *mediator) {
+  this->mediator = mediator;
+}
+
+void DataCollector::Notify(data_collector_event event) {
+  if (this->mediator != NULL) {
+    this->mediator->Update(event);
+  }
 }
 
 String DataCollector::getData(sensor_t sensor, void *sensor_data_type) {
@@ -20,60 +29,60 @@ String DataCollector::getData(sensor_t sensor, void *sensor_data_type) {
 }
 
 String DataCollector::getGPSData(void* gps_data_type) {
-  switch (getGPSDataType(gps_data_type)) {
+  gps_data_t data_type = *(gps_data_t*)(gps_data_type);
+  switch (data_type) {
     case gps_data_t::location:
       Serial.println("Getting GPS location...");
       return "35.897,-87.533";
+
     case gps_data_t::speed:
       Serial.println("Getting GPS speed...");
       return "529";
+
     case gps_data_t::date:
       Serial.println("Getting GPS date...");
       return "2020-02-03";
+
     case gps_data_t::time:
       Serial.println("Getting GPS time...");
       return "12:05:44";
+
     case gps_data_t::datetime:
       Serial.println("Getting GPS datetime...");
       return "2020-02-03,12:05:44";
+
     case gps_data_t::travel:
       Serial.println("Getting GPS travel...");
       return "2020-02-03,12:05:44,35.897,-87.533";
+
     default:
       return "";  // Unhandled Type
   }
 }
 
-gps_data_t DataCollector::getGPSDataType(void* sensor_data_type) {
-  gps_data_t gps_data_type = *(gps_data_t *)sensor_data_type;
-  return gps_data_type;
-}
-
 String DataCollector::getAccelerometerData(void* accelerometer_data_type) {
-  switch (getAccelerometerDataType(accelerometer_data_type)) {
+  accelerometer_data_t data_type = *(accelerometer_data_t*)accelerometer_data_type;
+  switch (data_type) {
     case accelerometer_data_t::x_acceleration:
       Serial.println("Getting accelerometer x acceleration...");
       return "0.229837";
+
     case accelerometer_data_t::y_acceleration:
       Serial.println("Getting accelerometer y acceleration...");
       return "-0.188201";
+
     case accelerometer_data_t::z_acceleration:
       Serial.println("Getting accelerometer z acceleration...");
       return "1.350662";
+
     default:
       return "";
   }
 }
 
-accelerometer_data_t DataCollector::getAccelerometerDataType(
-    void* sensor_data_type) {
-  accelerometer_data_t accelerometer_data_type =
-    *(accelerometer_data_t *)sensor_data_type;
-  return accelerometer_data_type;
-}
-
 String DataCollector::getEnvironmentalData(void* environmental_data_type) {
-  switch (getEnvironmentalDataType(environmental_data_type)) {
+  environmental_data_t data_type = *(environmental_data_t*)environmental_data_type;
+  switch (data_type) {
     case environmental_data_t::temperature:
       Serial.println("Getting environmental temperature...");
       return "72.898";
@@ -89,11 +98,4 @@ String DataCollector::getEnvironmentalData(void* environmental_data_type) {
     default:
       return "";
   }
-}
-
-environmental_data_t DataCollector::getEnvironmentalDataType(
-    void* sensor_data_type) {
-  environmental_data_t environmental_data_type =
-    *(environmental_data_t *)sensor_data_type;
-  return environmental_data_type;
 }
--- src/data_collector/data_collector.h
+++ src/data_collector/data_collector.h
@@ -1,16 +1,22 @@
-#ifndef _ROAD_SURFACE_DAQ_DATACOLLECTER_H
-#define _ROAD_SURFACE_DAQ_DATACOLLECTER_H
+#ifndef _ROAD_SURFACE_DAQ_DATA_COLLECTER_H
+#define _ROAD_SURFACE_DAQ_DATA_COLLECTER_H
 
 #include "Arduino.h"
+//#ifndef _ROAD_SURFACE_DAQ_DATA_COLLECTOR_EVENT_H
+#include "data_collector_event.h"
+//#endif
+//#ifndef _ROAD_SURFACE_DAQ_MEDIATOR_H
+//#include "mediator.h"
+//#endif
 // @require REQUIRES SENSOR LIBRARIES
 
-enum class sensor_t : char {  // reduces memory requirement
+enum class sensor_t : char {
   gps,
   accelerometer,
   enviormental
 };
 
-enum class gps_data_t : char {  // reduces memory requirement
+enum class gps_data_t : char {
   location,
   speed,
   date,
@@ -19,25 +25,32 @@ enum class gps_data_t : char {  // reduces memory requirement
   travel
 };
 
-enum class accelerometer_data_t : char {  // reduces memory requirement
+enum class accelerometer_data_t : char {
   x_acceleration,
   y_acceleration,
   z_acceleration
 };
 
-enum class environmental_data_t : char {  // reduces memory requirement
+enum class environmental_data_t : char {
   temperature,
   humidity,
   pressure,
   altitude
 };
 
-class DataCollector { // Implements Observable --> getState of sensors... gps connected? accelerometer interrupt latched? environmental data sampling timer complete?
-  public: // Interface for Mediator
+class Mediator;
+class DataCollector {
+  public:
     DataCollector();
+    void Attach(Mediator *mediator);
+    void Notify(data_collector_event event);
     String* getDataBuffer(sensor_t sensor, void *sensor_data_type);
     String getData(sensor_t sensor, void *sensor_data_type);
   private:
+    Mediator *mediator;
+    String getGPSData(void* gps_data_type);
+    String getAccelerometerData(void* accelerometer_data_type);
+    String getEnvironmentalData(void* environmental_data_type);
     // void updateCache(sensor_t sensor, void *sensor_data_type, void *cache);
     // GPS gps;
     // Accelerometer accelerometer;
@@ -45,12 +58,6 @@ class DataCollector { // Implements Observable --> getState of sensors... gps co
     // Date date_cache;
     // Time time_cache;
     // Acceleration acceleration_buffer[SIZE_ACCELERATION_BUFFER];
-    gps_data_t getGPSDataType(void *sensor_data_type);
-    String getGPSData(void* gps_data_type);
-    accelerometer_data_t getAccelerometerDataType(void* sensor_data_type);
-    String getAccelerometerData(void* accelerometer_data_type);
-    environmental_data_t getEnvironmentalDataType(void* sensor_data_type);
-    String getEnvironmentalData(void* environmental_data_type);
 };
 
 #endif
