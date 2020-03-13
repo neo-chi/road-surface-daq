09797eb456b76487931696bd02ed67019268a0f5
reece chimento <reecechimento@gmail.com>
2020-03-13 07:17:51 -0500

    entities(impact.h): add impact entity class
    The Impact class is responsible for recording information relevant to
    damaged roads collected by the data aqcuisition device.
    
    Impact implements the following interfaces:
    * Loggable: write impact information to file.
    * Uploadable: prepare https request information for upload to database.

---
+++ src/entities/impact/impact.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Arduino.h"
+#include "entities/loggable.h"
+#include "entities/uploadable.h"
+#include "entities/date.h"
+#include "entities/time.h"
+#include "entities/location.h"
+#include "entities/environment.h"
+#include "entities/acceleration.h"
+
+/*
+ * Impact - records data related to striking a damaged road.
+ *
+ * @log(): returns a String for file writing.
+ * @location_header(): returns a String detailing the upload location.
+ * @post(): returns a String of HTTP POST data.
+ *
+ * @gps_is_connected: boolean true if gps had sattelite connection.
+ * @DAQ_ID: unique data acquisition device identity.
+ * @date: date of data collection.
+ * @time: time of data collection.
+ * @location: google maps latitude and longitude.
+ * @speed: vehicle speed in mm/s.
+ * @acceleration: vehicle 3-axis acceleration in gs (gravity).
+ */
+class Impact : Loggable, Uploadable
+{
+        public:
+                String log();
+                String location_header();
+                String post();
+        private:
+                const bool gps_is_connected;
+                const int DAQ_ID;
+                date date;
+                location location;
+                time time;
+                float speed;
+                acceleration acceleration[512];
+};
