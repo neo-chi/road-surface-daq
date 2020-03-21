e4108227959888c789da637d588001ec55c8e86f
reece chimento <reecechimento@gmail.com>
2020-03-21 06:32:13 -0500

    src(storage): add driver, add mkdir
    The Storage contructor will try to initialize the SD card.
    
    The Storage::mkdir method has been tested and validated working.

--- test/storage/storage.ino
+++ test/storage/storage.ino
@@ -9,22 +9,22 @@ void setup()
         /** @test **/
         Storage storage;
         storage.mkdir("/today");
-        storage.mkdir("/today/impacts");
-        storage.write("/today/impacts/HHMMSSss.imp", "some impact data");
-        storage.mkdir("/uploaded");
-        String *out;
-        storage.read("/today/impacts/HHMMSSss.imp", out);
-        storage.mv("/today/impacts/HHMMSSss.imp", "/uploaded/HHMMSSss.imp");
-        storage.rm("/uploaded/HHMMSSss.imp");
+        //storage.mkdir("/today/impacts");
+        //storage.write("/today/impacts/HHMMSSss.imp", "some impact data");
+        //storage.mkdir("/uploaded");
+        //String *out;
+        //storage.read("/today/impacts/HHMMSSss.imp", out);
+        //storage.mv("/today/impacts/HHMMSSss.imp", "/uploaded/HHMMSSss.imp");
+        //storage.rm("/uploaded/HHMMSSss.imp");
 
 
-        const int buffer_len = 10;
-        String buffer[buffer_len];
-        for (int i = 0; i < buffer_len; i++) {
-                buffer[i] = "Message ";
-                buffer[i] += i;
-        }
-        storage.write("/today/travel/HHMMSS.trv", buffer, buffer_len);
+        //const int buffer_len = 10;
+        //String buffer[buffer_len];
+        //for (int i = 0; i < buffer_len; i++) {
+                //buffer[i] = "Message ";
+                //buffer[i] += i;
+        //}
+        //storage.write("/today/travel/HHMMSS.trv", buffer, buffer_len);
 }
 
 void loop()
