e44bbd9fad9ff040a08e2dfd1497812441e5d5f4
reece chimento <reecechimento@gmail.com>
2020-03-21 22:14:56 -0500

    src(devices/storage): add member methods
    The following methods have been implemented in the Storage class:
    * mkdir
    * rmdir
    * rm
    * mv
    * write(message)
    * write(buffer): NOTE buffer must be type uint8_t and may require
                     casting.
    * read

--- src/devices/storage.h
+++ src/devices/storage.h
@@ -11,10 +11,12 @@ class Storage
                 Storage();
                 void mkdir(char *path);
                 void mv(char *path_from, char *path_to);
+                void rename(char *path_from, char *path_to);
                 void rm(char *path);
+                void rmdir(char *path);
                 char **read(char *path);
                 void write(char *path, char *message);
-                void write(char *path, char **buffer, int buffer_size);
+                void write(char *path, uint8_t *buf, size_t bytes_to_write);
         private:
                 fs::FS &file_system;
 };
