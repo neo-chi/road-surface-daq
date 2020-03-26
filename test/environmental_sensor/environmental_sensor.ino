d32ce52a1ef60f3959c3f50b2a79b9c4949ab761
reece chimento <reecechimento@gmail.com>
2020-03-26 12:55:22 -0500

    src(environmental_sensor): add adafruit driver
    The adafruit driver has worked previously, however there appears to be a
    problem with either the sensor or wiring. In the meantime, until a new
    sensor arrives and may be tested, environmental data will not be
    implemented.

--- test/environmental_sensor/environmental_sensor.ino
+++ test/environmental_sensor/environmental_sensor.ino
@@ -1,21 +1,27 @@
 #include "environmental_sensor.h"
+#include "Wire.h"
+
+EnvironmentalSensor environmental_sensor;
+
 
 void setup()
 {
         // Setup debugging.
         delay(1000);  // UNIX
         Serial.begin(115200); // Debugging
-
         /** @test */
-        EnvironmentalSensor environmental_sensor;
-        environmental_data env_data = environmental_sensor.read();
-        printf("Temperature: \t %0.3lf\n", env_data.temperature);
-        printf("Humidity: \t %0.3lf\n", env_data.humidity);
-        printf("Pressure: \t %0.3lf\n", env_data.pressure);
-        printf("Altitude: \t %0.3lf\n", env_data.altitude);
+        Wire.begin();
+        environmental_sensor.driver.begin();
 }
 
 void loop()
 {
-
+        environmental_sensor.update_cache();
+        environmental_data env_data = environmental_sensor.read();
+        Serial.printf("----------------------\n");
+        Serial.printf("Temperature: \t %0.3lf\n", env_data.temperature);
+        Serial.printf("Humidity:    \t %0.3lf\n", env_data.humidity);
+        Serial.printf("Pressure:    \t %0.3lf\n", env_data.pressure);
+        Serial.printf("----------------------\n");
+        delay(500);
 }
