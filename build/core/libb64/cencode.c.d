586ed4efbe747bc7e8548c14cbf9d8699419138a
reece chimento <reecechimento@gmail.com>
2020-04-06 16:21:58 -0500

    remove platformio files

--- build/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-/home/neo/senior-design/build/core/libb64/cencode.c.o: \
- /home/neo/.arduino15/packages/esp32/hardware/esp32/1.0.4/cores/esp32/libb64/cencode.c \
- /home/neo/.arduino15/packages/esp32/hardware/esp32/1.0.4/cores/esp32/libb64/cencode.h
